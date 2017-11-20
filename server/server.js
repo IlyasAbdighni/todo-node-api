2267bd2c7e3b9087780dce8bc580837d9df26e54
Ilyas Abdighni <udunig@gmail.com>
2017-11-20 06:35:35 +0100

    added GET todos/id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var Todo = require('./models/Todo');
 var User = require('./models/User');
@@ -35,6 +36,23 @@ app.get('/todos', (req, res) => {
       res.status(400).send(err);
     }
   );
+});
+
+app.get('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send(null);
+  }
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(400).send();
+      }
+      res.send({ todo });
+    })
+    .catch(e => {
+      res.status(400).send();
+    });
 });
 
 app.listen(3000);
