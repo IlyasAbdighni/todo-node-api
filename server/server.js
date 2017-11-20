db79c8ee0846acdb00a7bf970f02a57f3ae1fedb
Ilyas Abdighni <udunig@gmail.com>
2017-11-20 09:11:44 +0100

    Added DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -55,6 +55,23 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(400).send();
+  }
+  Todo.findByIdAndRemove(id)
+    .then(todo => {
+      if (!todo) {
+        res.status(404).send();
+      }
+      res.status(200).send(todo);
+    })
+    .catch(err => {
+      res.status(400).send();
+    });
+});
+
 app.listen(3000);
 
 module.exports = { app };
